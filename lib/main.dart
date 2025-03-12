3c3fbe65578be1d224cdad32b559162371182ffa
B_M_Yashwanth <bhagyapushpagiri17@gmail.com>
2025-03-12 20:25:57 +0530

    Updates : firebase initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:safehome/firebase_options.dart';
 import 'package:safehome/login_signup/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
