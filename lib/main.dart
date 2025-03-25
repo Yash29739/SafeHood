0740f0459cfde51f34be95eba224c36f6ea49597
B_M_Yashwanth <bhagyapushpagiri17@gmail.com>
2025-03-25 20:20:52 +0530

    Updates: added a scrolldown feature

--- lib/main.dart
+++ lib/main.dart
@@ -70,29 +70,9 @@ class _SplashScreenState extends State<SplashScreen> {
                   .get();
 
           if (userDoc.exists) {
-            // ✅ Get role from Firestore
-            String role =
-                userDoc["role"] ?? "User"; // Default to "User" if null
-
-            // ✅ Navigate based on role
-            switch (role) {
-              case "Admin":
-                setState(() {
-                  nextScreen = AdminDashboard();
-                });
-                break;
-              case "Security":
-                setState(() {
-                  nextScreen = SecurityDashboard();
-                });
-                break;
-              case "User":
-              default:
-                setState(() {
-                  nextScreen = LandingScreen();
-                });
-                break;
-            }
+            setState(() {
+              nextScreen = LandingScreen();
+            });
           } else {
             // If user document doesn't exist, go to LoginScreen
             setState(() {
